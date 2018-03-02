0fdd2be8a9bd2086bd2884ea69d8083776f3341b
xuchunyan <xuchunyan@qianjiangcloud.com>
2018-03-02 11:19:39 +0800

    登录UI

--- src/pages/signin/App.vue
+++ src/pages/signin/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
   
-    <div class="login-top">  登录页logo</div>
+    <div class="login-top">
+      <img src="@/assets/images/logo.png" width="121" height="33">
+    </div>
     <div class="huiBg">
      <div class="main">
        <div class="left fl"><img src="@/assets/images/login-bg.png" width="520" height="429" />   </div>
@@ -20,10 +22,25 @@
           <div class="clearfix"></div>
           <div class="cont">
             <div class="ipt-warp">
-              <div class="icon1 el-icon-info">
-
-              </div>
+              <span class="icon1">
+                <img src="@/assets/images/login.png">
+              </span>
+              <input type="text" name="" placeholder="用户名/手机号码">
             </div>
+            <!-- ipt-warp end -->
+             <div class="ipt-warp">
+              <span class="icon1">
+                <img src="@/assets/images/lock.png">
+              </span>
+              <input type="password" name="" placeholder="请输入密码">
+            </div>
+            <!-- ipt-warp end -->
+            <div class="remember">
+              <input type="checkbox" name=""><span>记住用户名</span>
+              <a href="#">忘记密码</a>
+            </div>
+            <div class="signin-btn">登录</div>
+            <div class="register-btn">立即注册</div>
           </div>
           <!-- cont end -->
          </div>
@@ -32,6 +49,19 @@
        </div>
       <!-- right end -->
      </div>     
+    </div>
+    <!-- huiBg end -->
+    <div class="footer">
+      <div class="main">
+        <div class="footer-link">
+          <a href="">关于我们</a>
+          <a href="">联系我们</a>
+          <a href="">商家入驻</a>
+          <a href="">手机劲酒</a>
+          <p>沪ICP证070359号</p>
+          <p>Copyright&copy2017-2018&nbsp&nbsp千匠qianjiangcloud.com版权所有</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
