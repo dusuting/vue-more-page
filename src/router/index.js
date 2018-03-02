a89abf610c9a3287d5a82395ff66d144fb39159b
1752146907 <lirongzhi@qianjiangcloud.com>
2018-03-02 11:58:24 +0800

    注册成功等待组件

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import RegistrationType from '@/components/RegistrationType'
+import wait from '@/components/wait'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/register',
       name: 'RegistrationType',
       component: RegistrationType
+    },
+    {
+      path: '/wait',
+      name: 'wait',
+      component: wait
     }
   ]
 })
